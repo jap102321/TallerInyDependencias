b9a7818ea4ad0e751463001f7f1247765e5226f6
Juan Pablo Gonzalez <jupago14@gmail.com>
2023-08-13 11:49:50 -0500

    Starting repo, first exercise

---
+++ ejer1/src/main/java/com/ejer1/ejer1/Ejer1Application.java
@@ -0,0 +1,13 @@
+package com.ejer1.ejer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejer1Application.class, args);
+	}
+
+}
---
+++ ejer1/src/test/java/com/ejer1/ejer1/Ejer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejer1.ejer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
